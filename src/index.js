6347bd8656e5f7920433f10e7a50ce051a5e0adf
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-07-27 12:38:15 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,54 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  NavLink,
+} from "react-router-dom/cjs/react-router-dom.min";
+
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
+
+function NavBar() {
+  return (
+    <div>
+      <NavLink
+        to="/"
+        exact
+        style={linkStyles}
+        activeStyle={{ background: "darkblue" }}
+      >
+        Home
+      </NavLink>
+
+      <NavLink
+        to="/about"
+        exact
+        style={linkStyles}
+        activeStyle={{ background: "darkblue" }}
+      >
+        About
+      </NavLink>
+
+      <NavLink
+        to="/login"
+        exact
+        style={linkStyles}
+        activeStyle={{ background: "darkblue" }}
+      >
+        Login
+      </NavLink>
+    </div>
+  );
+}
 
 function Home() {
   return (
@@ -10,7 +59,52 @@ function Home() {
 }
 
 function App() {
-  return <Home />;
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
